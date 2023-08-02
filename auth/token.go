0d6603192a92a4080dbd9e1f9b68ba505bd2e5eb
Lancelot Owczarczak <lancelot@owczarczak.fr>
2023-08-02 16:08:38 +0200

    :zap: Auto create projectUser on FreeUserInit

--- auth/token.go
+++ auth/token.go
@@ -19,6 +19,18 @@ type ProjectUser struct {
 	ProjectID string `json:"project_id"`
 }
 
+type ProjectUserInsert struct {
+	AuthID    string `json:"auth_id"`
+	ProjectID string `json:"project_id"`
+}
+
+type Project struct {
+	ID           string `json:"id"`
+	Name         string `json:"name"`
+	AuthID       string `json:"auth_id"`
+	FreeUserInit bool   `json:"free_user_init"`
+}
+
 func GetAuthIdFromToken(token string) (string, error) {
 	supabaseUrl := os.Getenv("SUPABASE_URL")
 	supabaseKey := os.Getenv("SUPABASE_KEY")
@@ -37,15 +49,15 @@ func GetAuthIdFromToken(token string) (string, error) {
 	return user.ID, nil
 }
 
-func GetUserIdFromTokenProject(token string, project string) (string, error) {
+func GetUserIdFromTokenProject(token string, project string) (*string, error) {
 	auth_id, err := GetAuthIdFromToken(token)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 
 	client, err := db.CreateClient()
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 
 	var result *ProjectUser
@@ -57,10 +69,55 @@ func GetUserIdFromTokenProject(token string, project string) (string, error) {
 		Single().
 		ExecuteTo(&result)
 	if err != nil {
-		return "", err
+		return nil, nil
 	}
 
-	return result.ID, nil
+	return &result.ID, nil
+}
+
+func GetProjectByID(id string) (*Project, error) {
+	client, err := db.CreateClient()
+	if err != nil {
+		return nil, err
+	}
+
+	var result Project
+
+	_, err = client.From("projects").
+		Select("*", "exact", false).
+		Eq("id", id).
+		Single().
+		ExecuteTo(&result)
+	if err != nil {
+		return nil, nil
+	}
+
+	return &result, nil
+}
+
+func CreateProjectUser(token string, project_id string) (*string, error) {
+	auth_id, err := GetAuthIdFromToken(token)
+	if err != nil {
+		return nil, err
+	}
+
+	client, err := db.CreateClient()
+	if err != nil {
+		return nil, err
+	}
+
+	var result *ProjectUser
+
+	_, err = client.From("project_users").Insert(ProjectUserInsert{
+		AuthID:    auth_id,
+		ProjectID: project_id,
+	}, false, "", "", "exact").Single().ExecuteTo(&result)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &result.ID, nil
 }
 
 func TokenExchangeHandler(w http.ResponseWriter, r *http.Request, ps router.Params) {
@@ -88,8 +145,25 @@ func TokenExchangeHandler(w http.ResponseWriter, r *http.Request, ps router.Para
 		return
 	}
 
+	if user_id == nil {
+		project, err := GetProjectByID(project_id)
+		if err != nil {
+			http.Error(w, "500 Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+		if project.FreeUserInit == false {
+			http.Error(w, "403 forbidden", http.StatusForbidden)
+			return
+		}
+		user_id, err = CreateProjectUser(token, project_id)
+		if err != nil {
+			http.Error(w, "500 Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+	}
+
 	unsigned_user_token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-		"user_id": user_id,
+		"user_id": *user_id,
 	})
 
 	user_token, err := unsigned_user_token.SignedString([]byte(os.Getenv("JWT_SECRET")))
