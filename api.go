d093f0870b9bce3cdb97bf2c35bcb0b6525a437a
Lancelot Owczarczak <lancelot@owczarczak.fr>
2023-08-25 13:54:09 +0200

    :bug: Fix CORS Header

--- api.go
+++ api.go
@@ -4,8 +4,6 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/rs/cors"
-
 	httprouter "github.com/julienschmidt/httprouter"
 
 	auth "github.com/polyfact/api/auth"
@@ -18,39 +16,47 @@ import (
 	transcription "github.com/polyfact/api/transcription"
 )
 
+func CORS(
+	handler func(http.ResponseWriter, *http.Request, httprouter.Params),
+) func(http.ResponseWriter, *http.Request, httprouter.Params) {
+	return func(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Headers", "*")
+		handler(w, r, params)
+	}
+}
+
 func main() {
 	log.Print("Starting the server on :8080")
 
 	router := httprouter.New()
 
-	router.POST("/generate", middlewares.Auth(completion.Generate))
+	router.POST("/generate", CORS(middlewares.Auth(completion.Generate)))
 
-	router.GET("/chat/:id/history", middlewares.Auth(completion.GetChatHistory))
-	router.POST("/chats", middlewares.Auth(completion.CreateChat))
+	router.GET("/chat/:id/history", CORS(middlewares.Auth(completion.GetChatHistory)))
+	router.POST("/chats", CORS(middlewares.Auth(completion.CreateChat)))
 
-	router.POST("/transcribe", middlewares.Auth(transcription.Transcribe))
+	router.POST("/transcribe", CORS(middlewares.Auth(transcription.Transcribe)))
 
-	router.GET("/image/generate", middlewares.Auth(imageGeneration.ImageGeneration))
+	router.GET("/image/generate", CORS(middlewares.Auth(imageGeneration.ImageGeneration)))
 
-	router.GET("/memories", middlewares.Auth(memory.Get))
-	router.POST("/memory", middlewares.Auth(memory.Create))
-	router.PUT("/memory", middlewares.Auth(memory.Add))
-	router.GET("/project/:id/auth/token", auth.TokenExchangeHandler)
-	router.GET("/usage", middlewares.Auth(auth.UserRateLimit))
+	router.GET("/memories", CORS(middlewares.Auth(memory.Get)))
+	router.POST("/memory", CORS(middlewares.Auth(memory.Create)))
+	router.PUT("/memory", CORS(middlewares.Auth(memory.Add)))
+	router.GET("/project/:id/auth/token", CORS(auth.TokenExchangeHandler))
+	router.GET("/usage", CORS(middlewares.Auth(auth.UserRateLimit)))
 
-	router.GET("/kv", middlewares.Auth(kv.Get))
-	router.PUT("/kv", middlewares.Auth(kv.Set))
-	router.GET("/stream", middlewares.AuthStream(completion.Stream))
+	router.GET("/kv", CORS(middlewares.Auth(kv.Get)))
+	router.PUT("/kv", CORS(middlewares.Auth(kv.Set)))
+	router.GET("/stream", CORS(middlewares.AuthStream(completion.Stream)))
 
 	// Prompt Routes
-	router.GET("/prompt/name/:name", middlewares.Auth(prompt.GetPromptByName))
-	router.GET("/prompt/id/:id", middlewares.Auth(prompt.GetPromptById))
-	router.GET("/prompts", middlewares.Auth(prompt.GetAllPrompts))
-	router.POST("/prompt", middlewares.Auth(prompt.CreatePrompt))
-	router.PUT("/prompt/:id", middlewares.Auth(prompt.UpdatePrompt))
-	router.DELETE("/prompt/:id", middlewares.Auth(prompt.DeletePrompt))
+	router.GET("/prompt/name/:name", CORS(middlewares.Auth(prompt.GetPromptByName)))
+	router.GET("/prompt/id/:id", CORS(middlewares.Auth(prompt.GetPromptById)))
+	router.GET("/prompts", CORS(middlewares.Auth(prompt.GetAllPrompts)))
+	router.POST("/prompt", CORS(middlewares.Auth(prompt.CreatePrompt)))
+	router.PUT("/prompt/:id", CORS(middlewares.Auth(prompt.UpdatePrompt)))
+	router.DELETE("/prompt/:id", CORS(middlewares.Auth(prompt.DeletePrompt)))
 
-	handler := cors.AllowAll().Handler(router)
-
-	log.Fatal(http.ListenAndServe(":8080", handler))
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
