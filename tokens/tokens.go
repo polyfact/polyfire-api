101d30ebae9afd4841e870ab3447a65cbc93f8c0
Lancelot Owczarczak <lancelot@owczarczak.fr>
2023-09-18 15:34:42 +0200

    :racehorse: Initialize encoding at server start instead of at every countToken

--- tokens/tokens.go
+++ tokens/tokens.go
@@ -5,15 +5,20 @@ import (
 	tiktoken_loader "github.com/pkoukk/tiktoken-go-loader"
 )
 
-func CountTokens(_model string, text string) (int) {
+func initEncoding() *tiktoken.Tiktoken {
 	encoding := "cl100k_base"
-
 	tiktoken.SetBpeLoader(tiktoken_loader.NewOfflineLoader())
 	tke, err := tiktoken.GetEncoding(encoding)
 	if err != nil {
-		return 0
+		panic(err)
 	}
 
+	return tke
+}
+
+var tke = initEncoding()
+
+func CountTokens(_model string, text string) int {
 	token := tke.Encode(text, nil, nil)
 
 	return len(token)
